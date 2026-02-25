b491a3ee4ed627f9c499a62c6a2b2e3509ec055b
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-25 22:05:11 +0600

    let-const

---
+++ let-const.js
@@ -0,0 +1,26 @@
+const name = 'bangladesh';
+const variableName = 'BD_' + name;
+console.log(variableName);
+
+
+var name2 = 'bangladesh' //use na kora better
+
+const arr = [12, 45, 'i', 'my', '24'];
+// const arr = [12, 45, 'i', 'my', '24']; //new value assign not possible, as it is const 
+
+// value push kora jabe array te
+arr.push('hi');
+console.log(arr);
+
+
+// object 
+const student = {
+    name: 'anika',
+    id: 260,
+    age: 20
+}
+// alada vabe student ar declare kora jabe na 
+// value change kora jabe key er 
+student.name = 'panika';
+student.age = 22;
+console.log(student);
